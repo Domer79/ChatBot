d759036b2a4ce00807c1de641eac9424223e2f52
frolov.rvi <frolov.rvi>
2020-12-02 22:49:20 +0500

    CBF-8 заголовок в редакторе чата

--- front/ChatBot/src/app/chat-header/chat-header.component.ts
+++ front/ChatBot/src/app/chat-header/chat-header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-chat-header',
+  selector: 'chat-header',
   templateUrl: './chat-header.component.html',
   styleUrls: ['./chat-header.component.sass']
 })
