111636f98898118db445726537f4fc41f8649e09
frolov.rvi <frolov.rvi>
2021-02-04 16:50:21 +0500

    Отображение "Ничего не найдено" если не было не найдено ни одного вопроса

--- front/ChatBot/src/app/questions/search-questions/search-questions.component.ts
+++ front/ChatBot/src/app/questions/search-questions/search-questions.component.ts
@@ -1,10 +1,10 @@
-import {ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {QuestionsProviderService} from '../../services/questions-provider.service';
 import {Observable, Subscription} from 'rxjs';
 import Question from '../../../abstracts/Question';
 import {QuestionsComponent} from '../questions/questions.component';
 import {PageDispatcherService} from '../../services/page-dispatcher.service';
-import {map} from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 import Helper from '../../../misc/Helper';
 
 @Component({
@@ -14,23 +14,31 @@ import Helper from '../../../misc/Helper';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SearchQuestionsComponent implements OnInit, OnDestroy {
+  private searchQuerySubscription: Subscription;
+
+  @Input() searchQuery: string;
+  @Output() notFound: EventEmitter<any> = new EventEmitter<any>();
+
+  questions: Observable<Question[]>;
+  nothingToShow: boolean;
 
   constructor(
     private questionsProvider: QuestionsProviderService,
     private pageDispatcher: PageDispatcherService
   ) {
   }
-  @Input() searchQuery: string;
-  questions: Observable<Question[]>;
-  private searchQuerySubscription: Subscription;
-
 
   ngOnInit(): void {
-    this.questions = this.questionsProvider.searchQuestions(this.searchQuery);
+    this.questions = this.questionsProvider.searchQuestions(this.searchQuery).pipe(tap(questions => {
+      this.nothingToShow = questions.length === 0;
+    }));
     this.searchQuerySubscription = this.questionsProvider.searchQuery.pipe(map(query => {
       return Helper.searchQueryFilter(query);
     })).subscribe(query => {
-      this.questions = this.questionsProvider.searchQuestions(query);
+      this.nothingToShow = false;
+      this.questions = this.questionsProvider.searchQuestions(query).pipe(tap(questions => {
+          this.nothingToShow = questions.length === 0;
+      }));
     });
   }
 
